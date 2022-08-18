a88ba76979eb7c79a62015f7d70668ac367dfc06
Kairat Nurakhmet <foxykz@yandex.kz>
2022-08-18 18:59:45 +0200

    Logo-name changed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href='/'>Ecommerce shop</Link>
+        <Link href='/'>AMAZE Electronics</Link>
       </p>
 
       <button
